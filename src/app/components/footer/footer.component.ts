970d2f2be8caed4c2cab2e4012d161a34c22d17e
Samuel Trejo <trejomsamuel@gmail.com>
2024-08-09 15:09:30 -0500

    main functionality

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   template: `
     <footer>
       <p>TrejoClean Professional Cleaning Services, Copyright &copy; 2023</p>
+      <p>Call us at (615)944-4791, Or email us at trejomsamuel@gmail.com</p>
     </footer>
   `,
   styleUrls: ['./footer.component.scss']
